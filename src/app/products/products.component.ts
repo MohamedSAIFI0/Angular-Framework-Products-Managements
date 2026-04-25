9e54699e2beb5d6c835c5583b9ea315b1f1f0c41
mohamed-saifi <saifimsc@gmail.com>
2026-04-25 12:42:19 +0200

    add products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
   standalone: true
 })
 export class ProductsComponent {
+  products: Array<any> = [
+    {id: 1, name : "Computer", price: 2300, selected: true},
+    {id:2, name : "Printer", price: 1200, selected: false},
+    {id:3, name : "Smartphone", price: 1000, selected: true}
+
+  ]
 
 }
