aba6700524d25b24c027947a44e0239f0e142507
mohamed-saifi <saifimsc@gmail.com>
2026-04-25 12:56:14 +0200

    add ngOnInit Functions

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,21 +1,26 @@
-import { Component } from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-products',
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
   standalone: true
 })
-export class ProductsComponent {
-  products: Array<any> = [
-    {id: 1, name : "Computer", price: 2300, selected: true},
-    {id:2, name : "Printer", price: 1200, selected: false},
-    {id:3, name : "Smartphone", price: 1000, selected: true}
+export class ProductsComponent implements OnInit{
+  products! : Array<any>;
 
-  ]
+  constructor() {}
+  ngOnInit() {
+    this.products = [
+      {id: 1, name : "Computer", price: 2300, selected: true},
+      {id:2, name : "Printer", price: 1200, selected: false},
+      {id:3, name : "Smartphone", price: 1000, selected: true}
+    ]
+  }
 
 }
